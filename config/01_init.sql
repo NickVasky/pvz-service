fbcc73390cf22b7fb2f1d7d4d9b593ea7e027bfa
Nikita Vasilev <nickvasky@gmail.com>
2025-05-12 23:12:18 +0300

    Some SQL table schemas updated to simplify queries

--- config/01_init.sql
+++ config/01_init.sql
@@ -1,26 +1,30 @@
 CREATE TABLE IF NOT EXISTS roles (
-    id uuid primary key,
+    id serial primary key,
+    uuid uuid not null unique,
     name varchar(32)
 );
 
 CREATE TABLE IF NOT EXISTS statuses (
-    id uuid primary key,
+    id serial primary key,
+    uuid uuid not null unique,
     name varchar(64)
 );
 
 CREATE TABLE IF NOT EXISTS product_types (
-    id uuid primary key,
+    id serial primary key,
+    uuid uuid not null unique,
     name varchar(64)
 );
 
 CREATE TABLE IF NOT EXISTS cities (
-    id uuid primary key,
+    id serial primary key,
+    uuid uuid not null unique,
     name varchar(64)
 );
 
 CREATE TABLE IF NOT EXISTS users (
     id uuid primary key,
-    role_id uuid references roles(id),
+    role_id integer references roles(id),
     email varchar(320),
     password varchar(64)
 );
@@ -33,20 +37,20 @@ CREATE TABLE IF NOT EXISTS sessions (
 
 CREATE TABLE IF NOT EXISTS pvzs (
     id uuid primary key,
-    city_id uuid references cities(id),
+    city_id integer references cities(id),
     registration_date timestamp with time zone
 );
 
 CREATE TABLE IF NOT EXISTS receptions (
     id uuid primary key,
     pvz_id uuid references pvzs(id),
-    status_id uuid references statuses(id),
+    status_id integer references statuses(id),
     date_time timestamp with time zone
 );
 
 CREATE TABLE IF NOT EXISTS products (
     id uuid primary key,
-    type_id uuid references product_types(id),
+    type_id integer references product_types(id),
     reception_id uuid references receptions(id),
     date_time timestamp with time zone
 );
